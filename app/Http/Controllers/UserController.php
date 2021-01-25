00ca66703b5c38fa8fabc6a5853a9ea50d6c5662
Dhanushka Fernando <yohan7788@gmail.com>
2021-01-25 19:47:26 +0530

    completed user info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 
 class UserController extends Controller
 {
     //Show the users/form
     public function index()
     {
+
         return view('welcome');
     }
+
+    public function ajax(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = User::all()->map(function ($user)
+            {
+                return[
+                    'first_name'=>$user->first_name,
+                    'last_name'=>$user->last_name,
+                    'email'=>$user->email,
+                    'address'=>$user->address,
+                ];
+            });
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->make(true);
+        }
+
+
+    }
+
 
     //Store the user data
     public function store(Request $request)
     {
+        $validated=$request->validate(
+            [
+                'first_name'=>'required|alpha',
+                'last_name'=>'required|alpha',
+                'email'=>'required|email',
+                'address'=>'required|alpha',
+            ]
+        );
 
+        User::create($validated);
+        return redirect()->route('user.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 //Routes for user informations
 Route::get('/', [UserController::class,'index'])->name('user.index');
+Route::get('/ajax', [UserController::class,'ajax'])->name('user.ajax');
 
 
 Route::post('/user/store', [UserController::class,'store'])->name('user.store');
